3a5cc0f1b4558114f63cf72689974642f53cd56c
tarinrickett <trickett96@gmail.com>
2017-12-04 19:20:19 -0500

    BRRRRRRRRRAP

--- admin.php
+++ admin.php
@@ -54,18 +54,19 @@ session_start();
 
     	<!-- <a href="logout.php">logout</a> -->
 
-    	<table class="table table-striped table-bordered table-hover">
-    		<tr>
-    			<th scope="col">ID</th>
-    			<th scope="col">Name</th>
-    			<th scope="col">Email</th>
-    			<th scope="col">Favorite Framework</th>
-    			<th scope="col">Favorite Features</th>
-    			<th scope="col">Features to Change</th>
-    			<th scope="col">Suggested Features</th>
-    			<th scope="col">Suggested Framework</th>
-    			<th scope="col">Modify</th>
-    		</tr>
+			<div class="scrollable">
+	    	<table class="table table-striped table-bordered table-hover">
+	    		<tr>
+	    			<th scope="col">ID</th>
+	    			<th scope="col">Name</th>
+	    			<th scope="col">Email</th>
+	    			<th scope="col">Favorite Framework</th>
+	    			<th scope="col">Favorite Features</th>
+	    			<th scope="col">Features to Change</th>
+	    			<th scope="col">Suggested Features</th>
+	    			<th scope="col">Suggested Framework</th>
+	    			<th scope="col">Modify</th>
+	    		</tr>
 
 <?php
 	// 3. Use returned data (if any)
@@ -73,53 +74,54 @@ session_start();
 		// output data from each row
 ?>
 
-    		<tr scope="row" id="row<?php echo $frameworkSurvey['counter'];?>">
-    			<td id="counter_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['counter'];?></td>
-      			<td id="name_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['name'];?></td>
-      			<td id="email_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['email'];?></td>
-      			<td id="favoriteFramework_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['favoriteFramework'];?></td>
-      			 <td id="featuresToChange_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['featuresToChange'];?></td>
-      			<td id="favoriteFeature_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['favoriteFeature'];?></td>
-      			<td id="suggestionFeature_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['suggestionFeature'];?></td>
-      			<td id="suggestionFramework_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['suggestionFramework'];?></td>
+	    		<tr scope="row" id="row<?php echo $frameworkSurvey['counter'];?>">
+	    			<td id="counter_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['counter'];?></td>
+	      			<td id="name_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['name'];?></td>
+	      			<td id="email_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['email'];?></td>
+	      			<td id="favoriteFramework_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['favoriteFramework'];?></td>
+	      			 <td id="featuresToChange_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['featuresToChange'];?></td>
+	      			<td id="favoriteFeature_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['favoriteFeature'];?></td>
+	      			<td id="suggestionFeature_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['suggestionFeature'];?></td>
+	      			<td id="suggestionFramework_val<?php echo $frameworkSurvey['counter'];?>"><?php echo $frameworkSurvey['suggestionFramework'];?></td>
 
-      			<td>
-       				<button class="edit_button btn btn-primary" id="edit_button<?php echo $frameworkSurvey['counter'];?>" value="edit" onclick="edit_row('<?php echo $frameworkSurvey['counter'];?>');">Edit
-       				</button>
-       				<button  class="save_button btn btn-primary" id="save_button<?php echo $frameworkSurvey['counter'];?>" value="save" style="display:none" onclick="save_row('<?php echo $frameworkSurvey['counter'];?>');">Save
-       				</button>
-       				<!-- confirmation taken from http://www.javascripter.net/faq/confirm.htm -->
-       				<button class="delete_button btn btn-primary" id="delete_button<?php echo $frameworkSurvey['counter'];?>" value="delete" onclick="if(confirm('Are you sure you want to delete?'))delete_row('<?php echo $frameworkSurvey['counter'];?>'),alert('Record Deleted');">Delete
-       				</button>
-      			</td>
-     		</tr>
+	      			<td>
+	       				<button class="edit_button btn btn-primary" id="edit_button<?php echo $frameworkSurvey['counter'];?>" value="edit" onclick="edit_row('<?php echo $frameworkSurvey['counter'];?>');">Edit
+	       				</button>
+	       				<button  class="save_button btn btn-primary" id="save_button<?php echo $frameworkSurvey['counter'];?>" value="save" style="display:none" onclick="save_row('<?php echo $frameworkSurvey['counter'];?>');">Save
+	       				</button>
+	       				<!-- confirmation taken from http://www.javascripter.net/faq/confirm.htm -->
+	       				<button class="delete_button btn btn-primary" id="delete_button<?php echo $frameworkSurvey['counter'];?>" value="delete" onclick="if(confirm('Are you sure you want to delete?'))delete_row('<?php echo $frameworkSurvey['counter'];?>'),alert('Record Deleted');">Delete
+	       				</button>
+	      			</td>
+	     		</tr>
 
 
 
 
 <?php } ?>
 
-    	</table>
+	    	</table>
+			</div>
 
     	<div class="row">
     	   <a href="index.php">Back to Survey</a>
-    	  
+
     	</div>
 
     	<div class="row">
 						<a class="btn btn-primary" href="logout.php" role="button">Logout</a>
 		</div>
-    	
+
     </div>
 
     <!-- Downloading jQuery -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    
+
     <script type="text/javascript" src="js/modify_database.js"></script>
 
     <!-- Script for alert popups -->
     <script type="text/javascript">
-    	
+
     	$('form').on('click','.edit_button',function(){
 
     		$(save_button).show();
